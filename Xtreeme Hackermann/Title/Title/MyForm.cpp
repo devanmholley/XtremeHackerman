de71b081267a9aecf4594aca3b197dc9bbdd913c
ZBarron <zbb001@latech.edu>
2019-02-06 21:48:15 -0600

    TITLE SCREEN
    The Title screen works but It is currently unconnected to the rest of the UI

--- Xtreeme Hackermann/Title/Title/MyForm.cpp
+++ Xtreeme Hackermann/Title/Title/MyForm.cpp
@@ -1,4 +1,5 @@
 #include "MyForm.h"
+#include <stdlib.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
--- Xtreeme Hackermann/Title/Title/MyForm.h
+++ Xtreeme Hackermann/Title/Title/MyForm.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <stdlib.h>
+#include <stdio.h>
 namespace Title {
 
 	using namespace System;
@@ -34,6 +35,10 @@ namespace Title {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Label^  label1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +53,66 @@ namespace Title {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(879, 472);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Exit";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::SystemColors::Control;
+			this->button2->Location = System::Drawing::Point(410, 371);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(186, 55);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Begin";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Malgun Gothic", 48, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(128)),
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->label1->Location = System::Drawing::Point(209, 105);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(664, 86);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Xtreme Hackermann";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1004, 537);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->DoubleBuffered = true;
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		exit(0);
+	}
 	};
 }
