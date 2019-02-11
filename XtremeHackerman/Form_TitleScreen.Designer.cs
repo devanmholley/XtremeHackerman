3a677b50483e571fc4df27947b0d4d794f7554cf
Devan Holley <dmh032@latech.edu>
2019-02-10 23:16:08 -0600

    Initial commit to testing. Complete rewrite to C#. Contains functioning TitleScreen and placeholder Admin/Hacker forms.

---
+++ XtremeHackerman/Form_TitleScreen.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace XtremeHackerman
+{
+    partial class Form_TitleScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_TitleScreen));
+            this.label_TitleText = new System.Windows.Forms.Label();
+            this.tableLayoutPanel_Buttons = new System.Windows.Forms.TableLayoutPanel();
+            this.button_Admin = new System.Windows.Forms.Button();
+            this.button_Hacker = new System.Windows.Forms.Button();
+            this.button_Quit = new System.Windows.Forms.Button();
+            this.button_About = new System.Windows.Forms.Button();
+            this.tableLayoutPanel_Buttons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_TitleText
+            // 
+            this.label_TitleText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_TitleText.AutoSize = true;
+            this.label_TitleText.BackColor = System.Drawing.Color.Transparent;
+            this.label_TitleText.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_TitleText.ForeColor = System.Drawing.Color.White;
+            this.label_TitleText.Location = new System.Drawing.Point(191, 44);
+            this.label_TitleText.Name = "label_TitleText";
+            this.label_TitleText.Size = new System.Drawing.Size(725, 73);
+            this.label_TitleText.TabIndex = 0;
+            this.label_TitleText.Text = "XTREME HACKERMAN";
+            this.label_TitleText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel_Buttons
+            // 
+            this.tableLayoutPanel_Buttons.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel_Buttons.ColumnCount = 6;
+            this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel_Buttons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel_Buttons.Controls.Add(this.button_Admin, 1, 0);
+            this.tableLayoutPanel_Buttons.Controls.Add(this.button_Hacker, 3, 0);
+            this.tableLayoutPanel_Buttons.Controls.Add(this.button_Quit, 2, 2);
+            this.tableLayoutPanel_Buttons.Controls.Add(this.button_About, 2, 1);
+            this.tableLayoutPanel_Buttons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel_Buttons.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel_Buttons.Location = new System.Drawing.Point(0, 546);
+            this.tableLayoutPanel_Buttons.Name = "tableLayoutPanel_Buttons";
+            this.tableLayoutPanel_Buttons.RowCount = 4;
+            this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel_Buttons.Size = new System.Drawing.Size(1008, 183);
+            this.tableLayoutPanel_Buttons.TabIndex = 1;
+            // 
+            // button_Admin
+            // 
+            this.button_Admin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel_Buttons.SetColumnSpan(this.button_Admin, 2);
+            this.button_Admin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Admin.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Admin.ForeColor = System.Drawing.Color.White;
+            this.button_Admin.Location = new System.Drawing.Point(204, 3);
+            this.button_Admin.Name = "button_Admin";
+            this.button_Admin.Size = new System.Drawing.Size(296, 58);
+            this.button_Admin.TabIndex = 0;
+            this.button_Admin.Text = "ADMIN";
+            this.button_Admin.UseVisualStyleBackColor = true;
+            this.button_Admin.Click += new System.EventHandler(this.button_Admin_Click);
+            // 
+            // button_Hacker
+            // 
+            this.button_Hacker.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel_Buttons.SetColumnSpan(this.button_Hacker, 2);
+            this.button_Hacker.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Hacker.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Hacker.ForeColor = System.Drawing.Color.White;
+            this.button_Hacker.Location = new System.Drawing.Point(506, 3);
+            this.button_Hacker.Name = "button_Hacker";
+            this.button_Hacker.Size = new System.Drawing.Size(296, 58);
+            this.button_Hacker.TabIndex = 1;
+            this.button_Hacker.Text = "HACKER";
+            this.button_Hacker.UseVisualStyleBackColor = true;
+            this.button_Hacker.Click += new System.EventHandler(this.button_Hacker_Click);
+            // 
+            // button_Quit
+            // 
+            this.button_Quit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel_Buttons.SetColumnSpan(this.button_Quit, 2);
+            this.button_Quit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Quit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Quit.ForeColor = System.Drawing.Color.White;
+            this.button_Quit.Location = new System.Drawing.Point(456, 99);
+            this.button_Quit.Name = "button_Quit";
+            this.button_Quit.Size = new System.Drawing.Size(94, 26);
+            this.button_Quit.TabIndex = 3;
+            this.button_Quit.Text = "QUIT";
+            this.button_Quit.UseVisualStyleBackColor = true;
+            this.button_Quit.Click += new System.EventHandler(this.button_Quit_Click);
+            // 
+            // button_About
+            // 
+            this.button_About.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel_Buttons.SetColumnSpan(this.button_About, 2);
+            this.button_About.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_About.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_About.ForeColor = System.Drawing.Color.White;
+            this.button_About.Location = new System.Drawing.Point(456, 67);
+            this.button_About.Name = "button_About";
+            this.button_About.Size = new System.Drawing.Size(94, 26);
+            this.button_About.TabIndex = 2;
+            this.button_About.Text = "ABOUT";
+            this.button_About.UseVisualStyleBackColor = true;
+            // 
+            // Form_TitleScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.tableLayoutPanel_Buttons);
+            this.Controls.Add(this.label_TitleText);
+            this.MaximizeBox = false;
+            this.Name = "Form_TitleScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xtreme Hackerman";
+            this.Load += new System.EventHandler(this.Form_TitleScreen_Load);
+            this.tableLayoutPanel_Buttons.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_TitleText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Buttons;
+        private System.Windows.Forms.Button button_Admin;
+        private System.Windows.Forms.Button button_Hacker;
+        private System.Windows.Forms.Button button_Quit;
+        private System.Windows.Forms.Button button_About;
+    }
+}
---
+++ XtremeHackerman/Form_TitleScreen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace XtremeHackerman
+{
+    public partial class Form_TitleScreen : Form
+    {
+        // Define byte[] VCRFont as an embedded resource, to be used for custom font formatting
+        byte[] VCRFont = Properties.Resources.VCR_OSD_MONO_1_001;
+
+        public Form_TitleScreen()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Run when TitleScreen is first loaded.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form_TitleScreen_Load(object sender, EventArgs e)
+        {
+            // Set location & formatting for label_TitleText, including custom font
+            label_TitleText.Font = CustomFonts.GetFont(VCRFont, 48.0F); ;
+            label_TitleText.Location = new Point((this.Size.Width - label_TitleText.Size.Width) / 2, 50); // set location to X=[middle of screen],Y=50
+
+            // Set font format for each button on page, but retain existing font size
+            foreach(Control button in tableLayoutPanel_Buttons.Controls)
+            {
+                button.Font = CustomFonts.GetFont(VCRFont, button.Font.Size);
+            }
+        }
+
+        /// <summary>
+        /// Close TitlePage form. Will likely exit program as well.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Quit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Hide current TitlePage instance (NOT closing it) and load Admin form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Admin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            XtremeHackermanForms._formAdmin.ShowDialog();
+        }
+
+        /// <summary>
+        /// Hide current TitlePage instance (NOT closing it) and load Hacker form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Hacker_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            XtremeHackermanForms._formHacker.ShowDialog();
+        }
+
+    }
+}
