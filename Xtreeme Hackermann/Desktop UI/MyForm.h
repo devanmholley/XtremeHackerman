de13c18fba0d1bd97770fe09f172883e0b9077b0
gdavis1296 <34225206+gdavis1296@users.noreply.github.com>
2019-02-11 22:46:02 -0600

    Minor Changes to Resources

--- Xtreeme Hackermann/Desktop UI/MyForm.h
+++ Xtreeme Hackermann/Desktop UI/MyForm.h
@@ -36,10 +36,39 @@ namespace Project4 {
 		}
 	private: System::Windows::Forms::Button^  button1;
 	protected:
-	private: System::Windows::Forms::Button^  button2;
-	private: System::Windows::Forms::Button^  button3;
-	private: System::Windows::Forms::Button^  button4;
-	private: System::Windows::Forms::Button^  button5;
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+	protected:
+
+
+
+
 
 
 	protected:
@@ -59,75 +88,41 @@ namespace Project4 {
 		{
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button3 = (gcnew System::Windows::Forms::Button());
-			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->button5 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(0, 950);
+			this->button1->Location = System::Drawing::Point(402, 571);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(186, 85);
+			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Start";
+			this->button1->Text = L"button1";
 			this->button1->UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this->button2->Location = System::Drawing::Point(182, 950);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(186, 85);
-			this->button2->TabIndex = 1;
-			this->button2->Text = L"File Explorer";
-			this->button2->UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this->button3->Location = System::Drawing::Point(363, 950);
-			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(186, 85);
-			this->button3->TabIndex = 2;
-			this->button3->Text = L"Browser";
-			this->button3->UseVisualStyleBackColor = true;
-			// 
-			// button4
-			// 
-			this->button4->Location = System::Drawing::Point(546, 950);
-			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(186, 85);
-			this->button4->TabIndex = 3;
-			this->button4->Text = L"Command Line";
-			this->button4->UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this->button5->Location = System::Drawing::Point(729, 950);
-			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(186, 85);
-			this->button5->TabIndex = 4;
-			this->button5->Text = L"Email";
-			this->button5->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->ClientSize = System::Drawing::Size(1902, 1033);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->button4);
-			this->Controls->Add(this->button3);
-			this->Controls->Add(this->button2);
+			this->ClientSize = System::Drawing::Size(1426, 839);
 			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Start";
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
-	};
+	private: System::Void pictureBox1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void pictureBox1_Click_2(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		button1.Image = Properties.Resources.browsericon
+}
+};
 }
