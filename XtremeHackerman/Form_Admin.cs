7042e096c821458af1f9d557a67eeb8e21392a1b
ZBarron <zbb001@latech.edu>
2019-02-12 15:12:49 -0600

    Added Internet browser
    Currently, It has a forward, a back, a refresh, and a home button as well as a searchbar and a go button. There is currently nothing in the way of a portal yet.

--- XtremeHackerman/Form_Admin.cs
+++ XtremeHackerman/Form_Admin.cs
@@ -16,5 +16,26 @@ namespace XtremeHackerman
         {
             InitializeComponent();
         }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Shutdown_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void btn_Internet_Click(object sender, EventArgs e)
+        {
+            XtremeHackermanForms._formBrowser.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XtremeHackermanForms._formBrowser.ShowDialog();
+        }
     }
 }
