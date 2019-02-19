694a7698311e3efe51de590cb519f1e533dd2a17
Akastubaki <43384435+Akastubaki@users.noreply.github.com>
2019-02-18 19:20:15 -0600

    Add files via upload

--- XtremeHackerman/Form_Desktop.cs
+++ XtremeHackerman/Form_Desktop.cs
@@ -66,7 +66,7 @@ namespace XtremeHackerman
 
         private void shutdownToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Application.Exit();
         }
 
         private void internetBrowserToolStripMenuItem_Click(object sender, EventArgs e)
