b57314b0db4e889dc8162d5a361e829bf7abf598
Akastubaki <43384435+Akastubaki@users.noreply.github.com>
2019-03-09 19:04:40 -0600

    Task Manager Skeleton
    This is the base I am working with.  This will of course be changed and the reference to the code in question will be commentted.

---
+++ XtremeHackerman/TaskManager/TaskManager/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ProcessName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Runtime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Memory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ProcessName,
+            this.ID,
+            this.Runtime,
+            this.Memory});
+            this.listView1.Location = new System.Drawing.Point(-2, 26);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(803, 430);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ProcessName
+            // 
+            this.ProcessName.Text = "Process Name";
+            this.ProcessName.Width = 120;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // Runtime
+            // 
+            this.Runtime.Text = "Runtime";
+            // 
+            // Memory
+            // 
+            this.Memory.Text = "Memory Usage";
+            this.Memory.Width = 140;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ProcessName;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Runtime;
+        private System.Windows.Forms.ColumnHeader Memory;
+    }
+}
+
