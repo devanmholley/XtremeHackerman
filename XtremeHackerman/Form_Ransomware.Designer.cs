fb4ab6ffa8cc94729c8a78aaaaddd9e36014e2a1
ZBarron <zbb001@latech.edu>
2019-04-04 10:27:57 -0500

    Update
    Incremental update

--- XtremeHackerman/Form_Ransomware.Designer.cs
+++ XtremeHackerman/Form_Ransomware.Designer.cs
@@ -30,9 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Ransomware));
             this.label_Warning = new System.Windows.Forms.Label();
-            this.PB_Ransom_Icon = new System.Windows.Forms.PictureBox();
             this.Label_Destroy_Warning = new System.Windows.Forms.Label();
             this.TB_Ransomeware_Description = new System.Windows.Forms.TextBox();
+            this.PB_Ransom_Icon = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PB_Ransom_Icon)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,17 +49,6 @@
             this.label_Warning.Text = "Your personal files are encrypted!";
             this.label_Warning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // PB_Ransom_Icon
-            // 
-            this.PB_Ransom_Icon.BackgroundImage = global::XtremeHackerman.Properties.Resources.Ransomware_Icon;
-            this.PB_Ransom_Icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PB_Ransom_Icon.InitialImage = ((System.Drawing.Image)(resources.GetObject("PB_Ransom_Icon.InitialImage")));
-            this.PB_Ransom_Icon.Location = new System.Drawing.Point(13, 13);
-            this.PB_Ransom_Icon.Name = "PB_Ransom_Icon";
-            this.PB_Ransom_Icon.Size = new System.Drawing.Size(462, 468);
-            this.PB_Ransom_Icon.TabIndex = 1;
-            this.PB_Ransom_Icon.TabStop = false;
-            // 
             // Label_Destroy_Warning
             // 
             this.Label_Destroy_Warning.AutoSize = true;
@@ -73,6 +63,7 @@
             // 
             // TB_Ransomeware_Description
             // 
+            this.TB_Ransomeware_Description.BackColor = System.Drawing.Color.White;
             this.TB_Ransomeware_Description.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TB_Ransomeware_Description.Location = new System.Drawing.Point(491, 72);
             this.TB_Ransomeware_Description.Multiline = true;
@@ -80,6 +71,30 @@
             this.TB_Ransomeware_Description.ReadOnly = true;
             this.TB_Ransomeware_Description.Size = new System.Drawing.Size(702, 634);
             this.TB_Ransomeware_Description.TabIndex = 3;
+            this.TB_Ransomeware_Description.Text = resources.GetString("TB_Ransomeware_Description.Text");
+            // 
+            // PB_Ransom_Icon
+            // 
+            this.PB_Ransom_Icon.BackgroundImage = global::XtremeHackerman.Properties.Resources.Ransomware_Icon;
+            this.PB_Ransom_Icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PB_Ransom_Icon.InitialImage = ((System.Drawing.Image)(resources.GetObject("PB_Ransom_Icon.InitialImage")));
+            this.PB_Ransom_Icon.Location = new System.Drawing.Point(13, 13);
+            this.PB_Ransom_Icon.Name = "PB_Ransom_Icon";
+            this.PB_Ransom_Icon.Size = new System.Drawing.Size(462, 468);
+            this.PB_Ransom_Icon.TabIndex = 1;
+            this.PB_Ransom_Icon.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(181, 606);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Time Left!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form_Ransomware
             // 
@@ -87,6 +102,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(1205, 718);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.TB_Ransomeware_Description);
             this.Controls.Add(this.Label_Destroy_Warning);
             this.Controls.Add(this.PB_Ransom_Icon);
@@ -105,5 +121,6 @@
         private System.Windows.Forms.PictureBox PB_Ransom_Icon;
         private System.Windows.Forms.Label Label_Destroy_Warning;
         private System.Windows.Forms.TextBox TB_Ransomeware_Description;
+        private System.Windows.Forms.Label label1;
     }
 }
