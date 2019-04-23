69bc376ed195b540fcde7c6134c75216b0d42d14
Devan Holley <dmh032@latech.edu>
2019-04-22 22:59:25 -0500

    :construction: Added a terminal for the hacker desktop and classes for the hacker desktop and the terminal

---
+++ XtremeHackerman/Form_HackerTerminal.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace XtremeHackerman
+{
+    partial class Form_HackerTerminal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HackerTerminal_TXT = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // HackerTerminal_TXT
+            // 
+            this.HackerTerminal_TXT.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.HackerTerminal_TXT.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.HackerTerminal_TXT.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HackerTerminal_TXT.Location = new System.Drawing.Point(0, 0);
+            this.HackerTerminal_TXT.Multiline = true;
+            this.HackerTerminal_TXT.Name = "HackerTerminal_TXT";
+            this.HackerTerminal_TXT.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.HackerTerminal_TXT.Size = new System.Drawing.Size(933, 507);
+            this.HackerTerminal_TXT.TabIndex = 0;
+            // 
+            // Form_HackerTerminal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.HackerTerminal_TXT);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form_HackerTerminal";
+            this.Opacity = 0.75D;
+            this.Text = "Terminal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox HackerTerminal_TXT;
+    }
+}
