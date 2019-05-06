c0453c1bfab09970f9f254145b994fdc6779a3ca
gdavis1296 <34225206+gdavis1296@users.noreply.github.com>
2019-05-06 01:59:10 -0500

    Manual CLI Phish/RWare Tirggers + HELP commands
    Additional Refactoring

--- XtremeHackerman/Form_CLI.cs
+++ XtremeHackerman/Form_CLI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XtremeHackerman.Classes;
 
 namespace XtremeHackerman
 {
@@ -15,7 +16,7 @@ namespace XtremeHackerman
         string cliInput = null;
         string cliDirectory = "A:User\\HACKERMAN\\>";
         string cliCommand = null;
-        string introText = "HACKERMAN [Version 1.0.2] - COMMAND LINE INTERFACE - ";
+        string introText = "HACKERMAN [Version 1.9.2] - COMMAND LINE INTERFACE - ";
         int cliCurrentLine = 0;
 
         public Form_CLI()
@@ -32,7 +33,7 @@ namespace XtremeHackerman
                 if (cliCurrentLine == 0) //If we are initializing the Command Line interface
                 { // Initialize CLI with default command
                     cliText.Text = introText + "\r\n";
-                    cliText.AppendText("STARTING AT DEFAULT PATH || FOR A LIST OF COMMANDS VIEW (TBD) - PRESS ENTER TO BEGIN");
+                    cliText.AppendText("STARTING AT DEFAULT PATH || FOR A LIST OF COMMANDS TYPE HELP% - PRESS ENTER TO BEGIN");
                     cliText.Select(cliText.Text.Length, 0);
                     cliCurrentLine = 2;
                     return;
@@ -43,11 +44,18 @@ namespace XtremeHackerman
                 {
                     EventLogic.PhishingEmailAttack();
                 };
-                string msg = String.Format("You have entered: {0}", cliInput);
-                MessageBox.Show(msg, "COMMAND LINE ARGUMENT");
-                cliText.AppendText(cliDirectory);
+                //string msg = String.Format("You have entered: {0}", cliInput);
+                //MessageBox.Show(msg, "COMMAND LINE ARGUMENT");
+                //cliText.AppendText(cliDirectory);
                 cliCurrentLine += 1;
             }
+        if(e.KeyCode == Keys.Back)
+        {
+            if(cliText.Lines[cliCurrentLine].Length == 0)
+            {
+                e.SuppressKeyPress = true;
+            }
+        }
         }
 
         private void cliText_TextChanged(object sender, EventArgs e)
@@ -58,7 +66,7 @@ namespace XtremeHackerman
 
         private void checkCommand(string command)
         {
-            if (command.Contains("RUN "))
+            if (command.Contains("RUN%"))
             {
                 string[] action = command.Split(' ');
 
@@ -80,11 +88,11 @@ namespace XtremeHackerman
                 }
             }
 
-            if (command.Contains("DELETE "))
+            if (command.Contains("DELETE%"))
             {
                 // TBD: ONCE FILE MANAGEMENT IS IMPLEMENTED 
             }
-            if (command.Contains("DIR "))
+            if (command.Contains("DIR%"))
             {
                 string[] path = command.Split(' ');
                 string currentDir = path.Last();
@@ -92,7 +100,7 @@ namespace XtremeHackerman
                 cliText.AppendText(Environment.NewLine + cliDirectory);
                 cliCurrentLine += 1;
             }
-            if (command.Contains("ECHO "))
+            if (command.Contains("ECHO%"))
             {
                 string[] output = command.Split(' ');
                 foreach (string segment in output)
@@ -101,16 +109,44 @@ namespace XtremeHackerman
                 }
                 cliCurrentLine += output.Length;
             }
-            if (command.Contains("EXIT "))
+            if (command.Contains("EXIT%"))
             {
                 Application.Exit();
             }
-            if (command.Contains("IPCONFIG "))
+            if (command.Contains("IPCONFIG%"))
             {
                 cliText.AppendText(Environment.NewLine + "IP Address. . . . . . . . . . . . : 192.168.201.245");
                 cliText.AppendText(Environment.NewLine + "Subnet Mask . . . . . . . . . . . : 255.255.255.0");
                 cliText.AppendText(Environment.NewLine + "Default Gateway . . . . . . . . . : 192.168.201.1");
                 cliCurrentLine += 3;
+            }
+
+            if (command.Contains("HELP%"))
+            {
+                cliText.AppendText(Environment.NewLine + "---------------- COMMAND LINE INSTRUCTIONS ----------------");
+                cliText.AppendText(Environment.NewLine + "HERE YOU CAN ENTER A COMMAND FOR THE HACKERMAN SOFTWARE TO TRIGGER CERTAIN ACTIONS");
+                cliText.AppendText(Environment.NewLine + "TO TRIGGER A CERTAIN ACTION, ENTER THE KEYWORK FOLLOWED BY %");
+                cliText.AppendText(Environment.NewLine + "COMMANDS ARE LISTED BELOW");
+                cliText.AppendText(Environment.NewLine + "RUN_X - RUNS A SPECIFIC DESKTOP APPLICATION ");
+                cliText.AppendText(Environment.NewLine + "ECHO_X - ECHOS THE ENTERED TEXT ");
+                cliText.AppendText(Environment.NewLine + "EXIT - EXITS THE COMMAND LINE INTERFACE");
+                cliText.AppendText(Environment.NewLine + "IPCONFIG - DISPLAYS INTERNET PROTOCOL INFORMATION");
+                cliText.AppendText(Environment.NewLine + "-----------------------------------------------------------");
+                cliCurrentLine += 9;
+            }
+
+            if (command.Contains("PHISH%"))
+            {
+                Class_Progress.PhishSimulation();
+                cliText.AppendText(Environment.NewLine + "---------------- TRIGGERING PHISHING SIMULATION ----------------");
+                cliCurrentLine += 1;
+            }
+
+            if (command.Contains("RANSOMWARE%"))
+            {
+                Class_Progress.RansomwareSimulation();
+                cliText.AppendText(Environment.NewLine + "---------------- TRIGGERING RANSOMWARE SIMULATION ----------------");
+                cliCurrentLine += 1;
             }
         }
     }
